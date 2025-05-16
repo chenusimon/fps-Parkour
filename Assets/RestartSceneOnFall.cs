68eaafff61a3477ea5fda8f86b83583c95fb1e9f
chenusimon <49160235@est.ort.edu.ar>
2025-05-16 09:05:03 -0300

    Terminado

---
+++ Assets/RestartSceneOnFall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartSceneOnFall : MonoBehaviour
+{
+    public float deathFall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < deathFall)
+        {
+            SceneManager.LoadScene("SampleScene");
+            Debug.Log("respawn");
+        }
+
+
+    }
+}
