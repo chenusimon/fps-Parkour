45f57a8bd9f5af9d4aa2963b142891e21d89ad46
chenusimon <49160235@est.ort.edu.ar>
2025-05-30 09:05:26 -0300

    clase 30/5

---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int precio;
+    public PlayerMoney dineroManager;
+
+    void Start()
+    {
+        dineroManager = FindObjectOfType<PlayerMoney>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) 
+        {
+            dineroManager.UpdateMoney(precio);
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerMoney.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoney : MonoBehaviour
+{
+    public float availableMoney;
+
+    public void UpdateMoney(float amount)
+    {
+
+        if(availableMoney + amount < 0)
+        {
+            //impedir compra
+        }
+        else
+        {
+            availableMoney += amount;
+        }
+    }
+
+}
